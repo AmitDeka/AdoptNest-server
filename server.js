def4d019e0cc74cdd03bd386c9c47ddf58f7543c
AmitDeka <amitdeka49@gmail.com>
2025-06-15 22:47:09 +0530

    Update origin

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://adoptnest-client.onrender.com",
     credentials: true,
   })
 );
