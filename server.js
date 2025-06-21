d16ac111eb7ffd7ca873abc86f53249a56800bc8
AmitDeka <amitdeka49@gmail.com>
2025-06-21 22:20:51 +0530

    fix 3

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "https://adopt-nest.vercel.app",
+    origin: "https://adoptnest-client.onrender.com",
     credentials: true,
   })
 );
